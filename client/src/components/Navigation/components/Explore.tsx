dc88f6ba5e9ea202d21ce37b16b005531391e116
Bernard Calma <nard.calma@gmail.com>
2023-06-16 01:12:03 -0700

    refractor(Explore): Change mulitple divs and i to map
    Create solid icon array and use map to show multiple icon component

--- client/src/components/Navigation/components/Explore.tsx
+++ client/src/components/Navigation/components/Explore.tsx
@@ -1,44 +1,94 @@
 import { Icon } from "../../Common";
 
+const solidIcons: 
+    {
+        id: number,
+        containerClass: string,
+        icon: string,
+        text: string
+    }[] = [       
+        {
+            id: 0,
+            containerClass: "navIcons",
+            icon: "fire",
+            text: "Trending"
+        },
+        {
+            id: 1,
+            containerClass: "navIcons",
+            icon: "bag-shopping",
+            text: "Shopping"
+        },
+        {
+            id: 2,
+            containerClass: "navIcons",
+            icon: "music",
+            text: "Music"
+        },
+        {
+            id: 3,
+            containerClass: "navIcons",
+            icon: "tv",
+            text: "Movies & TV"
+        },
+        {
+            id: 4,
+            containerClass: "navIcons",
+            icon: "tower-broadcast",
+            text: "Live"
+        },
+        {
+            id: 5,
+            containerClass: "navIcons",
+            icon: "gamepad",
+            text: "Gaming"
+        },
+        {
+            id: 6,
+            containerClass: "navIcons",
+            icon: "newspaper",
+            text: "News"
+        },
+        {
+            id: 7,
+            containerClass: "navIcons",
+            icon: "trophy",
+            text: "Sports"
+        },
+        {
+            id: 8,
+            containerClass: "navIcons",
+            icon: "lightbulb",
+            text: "Learning"
+        },
+        {
+            id: 9,
+            containerClass: "navIcons",
+            icon: "shirt",
+            text: "Fashion & Beauty"
+        },
+        {
+            id: 10,
+            containerClass: "navIcons",
+            icon: "podcast",
+            text: "Podcast"
+        },
+    ]
+
 const Explore = () => {
     return(
         <div className="navExplore">
-            <Icon 
-                containerClass="navIcons"
-                icon="fire"
-                style="solid"
-                text="Trending"
-            />
-            <div className="navIcons">
-                <i className="fa-solid fa-bag-shopping"/> <span>Shopping</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-music"/><span>Music</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-tv"/><span>Movies & TV</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-tower-broadcast"/><span>Live</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-gamepad"/><span>Gaming</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-newspaper"/><span>News</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-trophy"/><span>Sports</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-lightbulb"/><span>Learning</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-shirt"/><span>Fashion & Beauty</span>
-            </div>
-            <div className="navIcons">
-                <i className="fa-solid fa-podcast"/><span>Podcast</span>
-            </div>
+            {
+                solidIcons.map(icon =>  
+                    <Icon 
+                        key = {icon.id}
+                        containerClass={icon.containerClass}
+                        icon = {icon.icon}
+                        style = "solid"
+                        text = {icon.text}
+                    />
+                )
+            }
         </div>
     )
 }
