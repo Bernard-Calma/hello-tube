b9144807167f9cfda50ff523aa5e71673385084a
Bernard Calma <nard.calma@gmail.com>
2023-07-23 17:15:37 -0700

    feat(Search Results): Create SearchResults view
    Create SearchResults folder in views, components and Styles.scss

---
+++ client/src/views/SearchResults/SearchResults.tsx
@@ -0,0 +1,11 @@
+import "./Styles.css"
+
+const SearchResults = () => {
+    return(
+        <div className="searchResultContainer">
+            Search Results
+        </div>
+    )
+}
+
+export default SearchResults;
