279ea68f8c298810e9582e50b61291c9a8a7b04d
Bernard-Calma <nard.calma@gmail.com>
2023-08-10 19:24:00 -0700

    feat(Library): Add p tag where to put videos
    Add p tags below title where video will be placed

--- client/src/views/Main/components/Library.tsx
+++ client/src/views/Main/components/Library.tsx
@@ -4,18 +4,22 @@ const Library = () => {
     return(<div>
         <div className="header">
             <h1 className="title">History</h1>
+            <p>Videos Here</p>
         </div>
         <VideoList />
         <div className="header">
             <h1 className="title">Watch Later</h1>
+            <p>Videos Here</p>
         </div>
         <VideoList />
         <div className="header">
             <h1 className="title">Playlists</h1>
+            <p>Videos Here</p>
         </div>
         <VideoList />
         <div className="header">
             <h1 className="title">Liked Videos</h1>
+            <p>Videos Here</p>
         </div>
         <VideoList />
     </div>)
