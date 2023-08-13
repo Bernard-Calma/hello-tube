d644e9acef3a7a76edca84029541569a98845c71
Bernard-Calma <nard.calma@gmail.com>
2023-08-13 00:24:33 -0700

    style(Library): Only show first 4 videos
    Add nth-child to show only 4 videos

--- client/src/views/Main/components/Library.tsx
+++ client/src/views/Main/components/Library.tsx
@@ -3,12 +3,13 @@ import VideoList from "./VideoList";
 const Library = () => {
     return(<div className="library">
         <div className="header">
+            <i className="fa-sharp fa-solid fa-clock-rotate-left"/>
             <h1 className="title">History</h1>
         </div>
         <VideoList />
         <div className="header">
+            <i className="fa-sharp fa-solid fa-clock-rotate-left"/>     
             <h1 className="title">Watch Later</h1>
-            <p>Videos Here</p>
         </div>
         <VideoList />
         <div className="header">
