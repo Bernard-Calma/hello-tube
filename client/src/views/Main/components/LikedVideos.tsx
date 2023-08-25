4ca576c32fe3f876435d65196666f21956bb0ec2
Bernard-Calma <nard.calma@gmail.com>
2023-08-24 23:59:17 -0700

    feat(Liked Videos): Add components
    Add videoThumbnail and videoPreview

--- client/src/views/Main/components/LikedVideos.tsx
+++ client/src/views/Main/components/LikedVideos.tsx
@@ -1,6 +1,36 @@
+import { VideoThumbnail } from "../../../components/Common";
+import { useAppSelector } from "../../../hooks";
+
 const LikedVideos = () => {
-    return <div>
-        <h1>Liked Videos</h1>
+    const {
+        videos
+    } = useAppSelector(store => store.youtube)
+    return <div className="likedVideos">
+        <div className="videoPreview">
+            {
+                videos.map((video, index) => index === 0 && 
+                    <VideoThumbnail 
+                        key = {video.etag}
+                        videoData = {video}
+                    />
+                )
+            }
+            <h2>Liked Videos</h2>
+            <p>Ryznrd</p>
+            <p>21 videos No Views Last Updated on 24 Aug 2023</p>
+            <i className="fa-solid fa-ellipsis-vertical"/>
+            <div>
+                <div>
+                    <i className="fa-solid fa-play"></i>
+                    <p>play all</p>
+                </div>
+                <div>
+                    <i className="fa-solid fa-shuffle"/>
+                   <p>shuffle</p> 
+                </div>
+            </div>
+        </div>
+
     </div>
 }
 
