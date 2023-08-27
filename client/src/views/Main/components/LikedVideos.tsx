1701742e4f14bbb4883538fa56d6dd3226144903
Bernard-Calma <nard.calma@gmail.com>
2023-08-26 20:28:21 -0700

    style(Liked Videos): Style video preview
    Added stlye on video preview

--- client/src/views/Main/components/LikedVideos.tsx
+++ client/src/views/Main/components/LikedVideos.tsx
@@ -1,5 +1,6 @@
 import { VideoThumbnail } from "../../../components/Common";
 import { useAppSelector } from "../../../hooks";
+import VideoList from "./VideoList";
 
 const LikedVideos = () => {
     const {
@@ -15,20 +16,25 @@ const LikedVideos = () => {
                     />
                 )
             }
-            <h2>Liked Videos</h2>
-            <p>Ryznrd</p>
-            <p>21 videos No Views Last Updated on 24 Aug 2023</p>
-            <i className="fa-solid fa-ellipsis-vertical"/>
-            <div>
-                <div>
-                    <i className="fa-solid fa-play"></i>
-                    <p>play all</p>
-                </div>
-                <div>
-                    <i className="fa-solid fa-shuffle"/>
-                   <p>shuffle</p> 
+            <div className="previewDetails">
+                <h2>Liked Videos</h2>
+                <p className="owner">Ryznrd</p>
+                <p>21 videos No Views Last Updated on 24 Aug 2023</p>
+                <i className="fa-solid fa-ellipsis-vertical"/>
+                <div className="navContainer">
+                    <div className="previewNav">
+                        <i className="fa-solid fa-play"></i>
+                        <p>Play all</p>
+                    </div>
+                    <div className="previewNav">
+                        <i className="fa-solid fa-shuffle"/>
+                    <p>Shuffle</p> 
+                    </div>
                 </div>
             </div>
+        </div>
+        <div className="likedVideosList">
+            <VideoList />
         </div>
 
     </div>
