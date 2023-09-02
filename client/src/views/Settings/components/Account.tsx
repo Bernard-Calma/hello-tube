4d312be7eaf5e4ea08af357e618984e87946db8d
Bernard-Calma <nard.calma@gmail.com>
2023-09-01 18:31:23 -0700

    style(Account): Add styles on account component
    Add style to account component

--- client/src/views/Settings/components/Account.tsx
+++ client/src/views/Settings/components/Account.tsx
@@ -1,32 +1,46 @@
 import "../Styles.css"
 
 const Account = () => <div className="accountSection">
-    <p>Account</p>
+    <div>
+        <p className="bold">Account</p>
+        <h2>Choose how you appear and what you see on HelloTube</h2>
+        <p>Signed in as bernard_calma@email.com</p> 
+    </div>
 
-    <h2>Choose how you appear and what you see on HelloTube</h2>
-    <p>Signed in as bernard_calma@email.com</p>
+    <div className="channel">
+        <h3 className="bold">Your HelloTube Channel</h3>
+        <p>This is your publick presence on HelloTube. You need a channgel to upload your own videos, comment on videos or create playlists.</p>
+        <div>
+            <p className="bold">Your channel</p>
+            <div className="subDiv">
+            <p className="user">Bernard Calma</p>
+                <p className="blue bold">Channel status and features</p>
+                <p className="blue bold">Add or manage your channel(s)</p>
+                <p className="blue bold">View advanced settings</p>
+            </div>
+        </div>
+    </div>
 
-    <hr />
-
-    <h3>Your HelloTube Channel</h3>
-    <p>This is your publick presence on HelloTube. You need a channgel to upload your own videos, comment on videos or create playlists.</p>
-
-    <p>Your channel</p>
-    <p>Bernard Calma</p>
-    <p>Channel status and features</p>
-    <p>Add or manage your channel(s)</p>
-    <p>View advanced settings</p>
-
-    <h3>Your Account</h3>
-    <p>You sign it to HelloTube with your email</p>
-    <p>Google Account</p>
-    <p>View or change your Email Account settings</p>
-    <p>You will be redirected to your Email Account page</p>
-
-    <p>Membership</p>
-    <p>No Membership</p>
-    <p>Get HelloTube Premium</p>
-    <p>HelloTube Premium offers uninterupted music, ad-free videos and more....</p>
+    <div className="account">
+        <h3>Your Account</h3>
+        <p>You sign it to HelloTube with your email</p>
+        <div>
+            <p className="bold">Google Account</p>
+            <div className="subDiv subDivAccount">
+                <p className="blue bold">View or change your Email Account settings</p>
+                <p>You will be redirected to your Email Account page</p>
+            </div>
+            
+        </div>
+        <div>
+            <p className="bold">Membership</p>
+            <div className="subDiv subDivAccount">
+                <p>No Membership</p>
+                <p className="blue bold">Get HelloTube Premium</p>
+                <p>HelloTube Premium offers uninterupted music, ad-free videos and more....</p> 
+            </div>
+          </div>
+      </div>
 </div>
 
 export default Account;
