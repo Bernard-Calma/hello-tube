f56d58207f434aec18d20741dfa3da14607e9169
Bernard-Calma <nard.calma@gmail.com>
2023-09-03 14:01:33 -0700

    feat(Billing): Complete billing page
    Complete billing page with style

--- client/src/views/Settings/components/Billing.tsx
+++ client/src/views/Settings/components/Billing.tsx
@@ -4,8 +4,8 @@ const Billing = () => {
             <h3>Billing and payments</h3>
             <h2>Choose how you make purchases on HelloTube</h2>
 
-            <div>
-                <p>Quick purchase is disabled</p>
+            <div style={{borderTop: "0.5px solid gray"}}>
+                <p className="bold">Quick purchase is disabled</p>
                 <p>You will asked to verify your account for all HelloTube purchases</p>
                 <p className="blue">Learn more about purchase verification</p>
             </div>
